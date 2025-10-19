5a0c8c94d142313d8fb2d4da62eadc2d3026df80
Avneesh0404 <ajarangal_be22@thapar.edu>
2025-10-19 17:13:25 +0530

    done last check

--- client/src/context/socket.jsx
+++ client/src/context/socket.jsx
@@ -1,18 +1,17 @@
 import React, { createContext, useContext, useMemo, useEffect } from "react";
 import { io } from "socket.io-client";
-
 export const socketContext = createContext(null);
 
 export const useSocket = () => {
   const socket = useContext(socketContext);
   return socket;
 };
-
+const url = process.env.REACT_APP_SOCKET_URL
 export const SocketProvider = (props) => {
   // ensure protocol + proper URL and add any options you need
   const socket = useMemo(
     () =>
-      io("http://localhost:8000", {
+      io(url, {
         transports: ["websocket"],
         autoConnect: true,
       }),
